5f38ce3ce35e9ef61d85ef69045812e75c3683cd
Brendyn Montgomery <brendyn@avocadesign.co.nz>
2013-04-16 12:13:58 +1200

    added site credits function to functions.php
    Signed-off-by: Brendyn Montgomery <brendyn@avocadesign.co.nz>

--- functions.php
+++ functions.php
@@ -32,10 +32,27 @@ function torlesse_theme_styles()
   // then dependencies and ver no. and media type)
   wp_register_style( 'torlesse-main', 
     get_stylesheet_directory_uri() . '/css/torlesse.css', 
-    array()
+    array() // Input any CSS dependncies in this array
     );
 
   // enqueing:
   wp_enqueue_style( 'torlesse-main' );
 }
 add_action('wp_enqueue_scripts', 'torlesse_theme_styles');
+
+/** Customize the site credits */
+add_filter( 'genesis_footer_creds_text', 'torlesse_footer_creds_text' );
+function torlesse_footer_creds_text() {
+	$torlesse_developer_link_alt = 'Website Design, WordPress web development and hosting, Genesis framework';
+    echo '<div class="creds">'; //enclose the site credits in a div.creds
+    echo '<div class="site-copyright"><p>'; //display the copyright info in a div.site-copyright
+    echo '&copy; ';
+    echo bloginfo('name'); // Display the site name in the copyright information
+    echo ' ';
+    echo date('Y');
+    echo '</p></div>'; // End .site-copyright
+    echo '<div class="website-designed-by"><p>'; // Display the Developer credits in a div.website-designed-by
+    echo ' Website Designed by <strong><a href="' . CHILD_THEME_URL . '" class="website-designed-by-link" alt="' . $torlesse_developer_link_alt . '" title="' . $torlesse_developer_link_alt . '">' . CHILD_THEME_NAME . '</a></strong>';
+    echo '</p></div>'; // End .website-designed-by
+    echo '</div>'; // End .creds
+}
